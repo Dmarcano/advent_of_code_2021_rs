b7b440ea6d61efe7714219afe4c5d16fadf9e3f0
Diego Marcano <marcanp.diego@gmail.com>
2021-12-02 01:31:16 -0800

    cargo fmt for day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -8,37 +8,35 @@ enum Directions {
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-struct AimSubmarineLoc { 
-    pub depth : i32, 
-    pub x : i32,
-    pub aim: i32, 
+struct AimSubmarineLoc {
+    pub depth: i32,
+    pub x: i32,
+    pub aim: i32,
 }
 
 impl Add<Directions> for AimSubmarineLoc {
     type Output = Self;
 
     fn add(self, rhs: Directions) -> Self::Output {
-
         match rhs {
-            Directions::Up(aim_decrease) => Self { 
-                depth: self.depth, 
-                x: self.x, 
-                aim: self.aim - aim_decrease, 
+            Directions::Up(aim_decrease) => Self {
+                depth: self.depth,
+                x: self.x,
+                aim: self.aim - aim_decrease,
             },
-            Directions::Down(aim_increase) => Self { 
-                depth: self.depth, 
+            Directions::Down(aim_increase) => Self {
+                depth: self.depth,
                 x: self.x,
                 aim: self.aim + aim_increase,
             },
-            Directions::Forward(motion_amnt) => Self { 
-                depth: self.depth +  (self.aim * motion_amnt), 
+            Directions::Forward(motion_amnt) => Self {
+                depth: self.depth + (self.aim * motion_amnt),
                 x: self.x + motion_amnt,
                 aim: self.aim,
             },
         }
     }
 }
-
 
 /// A submarine location for part one of day two for the advent of code 2021
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
@@ -71,13 +69,13 @@ impl Add<Directions> for SubmarineLoc {
 
 impl Directions {
     /// A line of the input file is a string with a number separated by a space.
-    /// 
+    ///
     /// The string is either "forward", "up", or "down".
-    /// 
+    ///
     /// This method parses one such line into a valid Directions enum.
-    pub fn from_input_line(line : &str) -> Result<Self, &'static str> {
+    pub fn from_input_line(line: &str) -> Result<Self, &'static str> {
         let mut split = line.split_whitespace();
-        let direction = match split.next().unwrap() { 
+        let direction = match split.next().unwrap() {
             "up" => Directions::Up(split.next().unwrap().parse::<i32>().unwrap()),
             "down" => Directions::Down(split.next().unwrap().parse::<i32>().unwrap()),
             "forward" => Directions::Forward(split.next().unwrap().parse::<i32>().unwrap()),
@@ -95,47 +93,72 @@ pub fn part_one(input: String) -> String {
         .map(|x| Directions::from_input_line(x).unwrap())
         .collect::<Vec<_>>();
 
-        let final_loc = input_lines.iter().fold(loc, |acc, x| acc + *x);
-        (final_loc.x * final_loc.depth).to_string()
+    let final_loc = input_lines.iter().fold(loc, |acc, x| acc + *x);
+    (final_loc.x * final_loc.depth).to_string()
 }
 
 pub fn part_two(input: String) -> String {
-    let loc = AimSubmarineLoc { depth: 0, x: 0, aim: 0 };
+    let loc = AimSubmarineLoc {
+        depth: 0,
+        x: 0,
+        aim: 0,
+    };
 
     let input_lines = input
         .lines()
         .map(|x| Directions::from_input_line(x).unwrap())
         .collect::<Vec<_>>();
 
-        let final_loc = input_lines.iter().fold(loc, |acc, x| acc + *x);
-        (final_loc.x * final_loc.depth).to_string()
-
+    let final_loc = input_lines.iter().fold(loc, |acc, x| acc + *x);
+    (final_loc.x * final_loc.depth).to_string()
 }
-
 
 #[cfg(test)]
 
-mod test { 
+mod test {
     use super::*;
 
     #[test]
-    fn test_parse_line_directions() { 
+    fn test_parse_line_directions() {
         assert_eq!(Directions::from_input_line("up 1"), Ok(Directions::Up(1)));
-        assert_eq!(Directions::from_input_line("down 6"), Ok(Directions::Down(6)));
-        assert_eq!(Directions::from_input_line("forward 10"), Ok(Directions::Forward(10)));
+        assert_eq!(
+            Directions::from_input_line("down 6"),
+            Ok(Directions::Down(6))
+        );
+        assert_eq!(
+            Directions::from_input_line("forward 10"),
+            Ok(Directions::Forward(10))
+        );
     }
 
     #[test]
-    fn add_directions_test() { 
+    fn add_directions_test() {
         let loc = SubmarineLoc { depth: 0, x: 0 };
         assert_eq!(loc + Directions::Down(1), SubmarineLoc { depth: 1, x: 0 });
-        assert_eq!(loc + Directions::Forward(1), SubmarineLoc { depth: 0, x: 1 });
-        assert_eq!(loc + Directions::Down(5) + Directions::Up(3) + Directions::Forward(10), SubmarineLoc { depth: 2, x: 10 });
+        assert_eq!(
+            loc + Directions::Forward(1),
+            SubmarineLoc { depth: 0, x: 1 }
+        );
+        assert_eq!(
+            loc + Directions::Down(5) + Directions::Up(3) + Directions::Forward(10),
+            SubmarineLoc { depth: 2, x: 10 }
+        );
     }
 
     #[test]
-    fn submarine_aim_addition_test() { 
-        let loc = AimSubmarineLoc { depth: 0, x: 0, aim: 0 };
-        assert_eq!(loc + Directions::Forward(5) + Directions::Down(5) + Directions::Forward(8), AimSubmarineLoc { depth: 40, x: 13, aim: 5 });
+    fn submarine_aim_addition_test() {
+        let loc = AimSubmarineLoc {
+            depth: 0,
+            x: 0,
+            aim: 0,
+        };
+        assert_eq!(
+            loc + Directions::Forward(5) + Directions::Down(5) + Directions::Forward(8),
+            AimSubmarineLoc {
+                depth: 40,
+                x: 13,
+                aim: 5
+            }
+        );
     }
 }
