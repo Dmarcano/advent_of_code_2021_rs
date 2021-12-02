eec6359dc48a0fd0e02ab29f52a013d69ac92eed
Diego Marcano <marcanp.diego@gmail.com>
2021-12-02 01:46:48 -0800

    day two done

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,5 +1,16 @@
+//! This module is part of the second day of advent of code. This involves the submarine problem in which
+//! a submarine is given a set of directions to take to move the submarine by changing its depth and
+//! x offset from the origin. Then at the final destination one must multiply the final depth and final x offset
+//!
+//!
+//! To solve this problem we define the possible directions that can be given as an Enum Algabreaic Data Type (ADT).
+//! We then define a struct that contains the depth and x offset of the submarine. We then implement adding a direction
+//! to the submarine so that we can simply use Rust's iterators to solve the problem.
+//!
+
 use std::ops::Add;
 
+/// The possible directions that can be given to the submarine as specified in the problem.
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Directions {
     Up(i32),
@@ -7,6 +18,8 @@ enum Directions {
     Forward(i32),
 }
 
+/// The second submarine problem adds an aim portion with slight changes to how the submarine behaves with
+/// given Directions.
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct AimSubmarineLoc {
     pub depth: i32,
@@ -17,6 +30,13 @@ struct AimSubmarineLoc {
 impl Add<Directions> for AimSubmarineLoc {
     type Output = Self;
 
+    /// Adds a direction to the submarine location to the following rules
+    ///
+    /// 1. Up(amnt) **decreases** the submarine AIM by the amnt
+    /// 2. Down(amnt) **increases** the submarine AIM by the amnt
+    /// 3. Forward(amnt) **moves** the submarine forward(x) by the amnt
+    ///  AND **increases** the submarine depth by aim*amnt
+    ///
     fn add(self, rhs: Directions) -> Self::Output {
         match rhs {
             Directions::Up(aim_decrease) => Self {
@@ -48,6 +68,12 @@ struct SubmarineLoc {
 impl Add<Directions> for SubmarineLoc {
     type Output = Self;
 
+    /// Adds a direction to the submarine location to the following rules
+    ///
+    /// 1. Up(amnt) **decreases** the submarine depth by the amnt
+    /// 2. Down(amnt) **increases** the submarine depth by the amnt
+    /// 3. Forward(amnt) **moves** the submarine forward(x) by the amnt
+    ///
     fn add(self, other: Directions) -> Self {
         match other {
             Directions::Up(depth_decrease) => Self {
@@ -85,31 +111,35 @@ impl Directions {
     }
 }
 
-pub fn part_one(input: String) -> String {
-    let loc = SubmarineLoc { depth: 0, x: 0 };
-
+/// This function is a generic function over a type T
+///
+/// The type T is bounded over the Trait Add<Direcitons, Output = T> meaning that the Type must implement
+/// the Add trait with the Directions type as the input and the type T as the output.
+fn submarine_problem<T: Add<Directions, Output = T>>(input: String, submarine: T) -> T {
     let input_lines = input
         .lines()
         .map(|x| Directions::from_input_line(x).unwrap())
         .collect::<Vec<_>>();
 
-    let final_loc = input_lines.iter().fold(loc, |acc, x| acc + *x);
+    input_lines.iter().fold(submarine, |acc, x| acc + *x)
+}
+
+/// solves the submarine problem for the first submarine type: depth and x offset
+pub fn part_one(input: String) -> String {
+    let loc = SubmarineLoc { depth: 0, x: 0 };
+
+    let final_loc = submarine_problem(input, loc);
     (final_loc.x * final_loc.depth).to_string()
 }
 
+/// solves the submarine problem for the second submarine type: depth, x offset, and aim
 pub fn part_two(input: String) -> String {
     let loc = AimSubmarineLoc {
         depth: 0,
         x: 0,
         aim: 0,
     };
-
-    let input_lines = input
-        .lines()
-        .map(|x| Directions::from_input_line(x).unwrap())
-        .collect::<Vec<_>>();
-
-    let final_loc = input_lines.iter().fold(loc, |acc, x| acc + *x);
+    let final_loc = submarine_problem(input, loc);
     (final_loc.x * final_loc.depth).to_string()
 }
 
