07d6ad8fb88aa11961746f9d32c3c9aa8744cc2d
Diego Marcano <marcanp.diego@gmail.com>
2021-12-01 20:20:14 -0800

    advent of code day 1 part 1 done

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,12 +1,26 @@
+/// Every line of input contains a single integer.
+///
+/// The goal is to count the number of times the subsequent numbers are larger than the previous one.
+///  ignoring the first one since there are no values to satisfy the comparison.
 pub fn part_one(input: String) -> String {
     let input_lines = input
         .lines()
         .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
-    println!("{:#?}", input_lines);
-    todo!("Havent done!")
+
+    let mut count = 0;
+
+    input_lines.iter().reduce(|val_1, val_2| {
+        if val_1 < val_2 {
+            count += 1;
+        }
+        return val_2;
+    });
+    println!("{:#?}", count);
+
+    count.to_string()
 }
 
 pub fn part_two(input: String) -> String {
-    todo!("Havent done part one yet!")
+    "Havent done part one yet!".to_string()
 }
